68f8d8f80e0b2388795bb9fb2983f00f2811e4dc
Dylantd <95030738+Dylantd@users.noreply.github.com>
2021-11-25 19:22:41 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
+  text1: "He luu Tố Như !",
+  text2: "Em có điều này muốn hỏi nên chị nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Chị thích em có phải không nào ._.",
+  text4: "Nếu chị ko trả lời mà thoát ra tức là chị trẻ trâu =)))))) ",
+  text5: "Nchu không có ",
+  text6: "Thích ơi là thích <3",
+  text7: "lí do chị thích em đi :vvvv",
   text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text9: "Vì em dth vlllll",
+  text10: "Em biết mà ^^ Yêu chị 300.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Thấy em vippro max khong?=)))))",
+  text12: "Đỉnh",
 };
 
 $(document).ready(function () {
